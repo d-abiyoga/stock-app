c12fe3d99f50a7879dd39c94599dfd9e0cccb23d
Dimas Abiyoga <dims@pop-os.localdomain>
2021-08-08 19:40:57 +0700

    apply different step of current price for each range (bug: yes)

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 console.log("App.js has been loaded");
 
 // Input
-const currentAveragePriceHTML = document.getElementById("currentSharePrice");
+const currentAveragePriceHTML = document.getElementById("currentAveragePrice");
 const currentShareQuantityHTML = document.getElementById("currentShareQuantity"); 
 const buyingPriceHTML = document.getElementById("buyingPrice"); 
 const buyingQuantityHTML = document.getElementById("buyingQuantity"); 
@@ -61,12 +61,128 @@ function printAveragePrice(averagePrice) {
 }
 
 function printEquity(currentEquity, additionalEquity, totalEquity) {
-    currentEquityHTML.innerHTML = `Rp. ${currentEquity}`;
-    additionalEquityHTML.innerHTML = `Rp. ${additionalEquity}`;
-    totalEquityHTML.innerHTML = `Rp. ${totalEquity}`;
+    currentEquityHTML.innerHTML = `Rp. ${new Intl.NumberFormat().format(currentEquity)}`;
+    additionalEquityHTML.innerHTML = `Rp. ${new Intl.NumberFormat().format(additionalEquity)}`;
+    totalEquityHTML.innerHTML = `Rp. ${new Intl.NumberFormat().format(totalEquity)}`;
 }
 
 function printPotGL(currentPotGL, newPotGL) {
     currentPotGLHTML.innerHTML = `${currentPotGL.toFixed(2)}%`;
     newPotGLHTML.innerHTML = `${newPotGL.toFixed(2)}%`;
 }
+
+
+
+
+// Under development
+/*
+    Apply share price fraction based on idx regulation on https://www.idx.co.id/investor/mekanisme-perdagangan/
+    Kelompok Kerja (Rp)          |   Fraksi Harga           | Maksimum Perubahan
+    <200                                     |    Rp. 1                        |   Rp 10
+    200 - 500                             |    Rp. 2                        |   Rp 20
+    500 - 2000                           |    Rp. 5                        |   Rp 50
+    2000 - 5000                         |    Rp. 10                      |   Rp 100
+    >= 5000                                |    Rp. 25                      |   Rp 250
+    Known bug: in transition number such as 200, 500, 2000, 5000
+
+ */
+
+    buyingPriceHTML.addEventListener("change" , (e) => {
+    console.log(e.target);
+    console.log(e.target.step);
+    console.log(e);
+
+    // Set step (share price fraction) value based on input price 
+    if (e.target.value < 200) {
+        return e.target.step = 1;
+    } 
+    // break value at 200
+    else if (e.target.value == 200) {
+        if (e.key == "ArrowUp") // e.target.value = parseInt(e.target.value) + 1;
+        return e.target.step = 2;
+    }  
+    else if (e.target.value < 500) {
+        return e.target.step = 2;
+    } 
+    //break value at 500
+    else if (e.target.value == 500) {
+        if (e.key == "ArrowDown") return e.target.step = 2;
+        return e.target.step = 5;
+    }
+
+    //break value at 2000
+    else if (e.target.value == 2000) {
+        if (e.key == "ArrowDown") return e.target.step = 5;
+        return e.target.step = 10;
+    }
+
+    else if (e.target.value < 2000) {
+        return e.target.step = 5;
+    } 
+
+    //break value at 5000
+    else if (e.target.value == 5000) {
+        if (e.key == "ArrowDown") return e.target.step = 5;
+        return e.target.step = 10;
+    }
+    else if (e.target.value < 5000) {
+        return e.target.step = 10;
+    } 
+
+    else if (e.target.value > 5000 ) {
+        return e.target.step = 25;
+    } else {
+        console.log("something error");
+    }
+});
+
+// By Using "keyup" as below, we can check the key pressed
+
+// buyingPriceHTML.addEventListener("keyup", (e) => {
+//     console.log(e.target);
+//     console.log(e.target.step);
+//     console.log(e);
+
+//     // Set step (share price fraction) value based on input price 
+//     if (e.target.value < 200) {
+//         return e.target.step = 1;
+//     } 
+//     // break value at 200
+//     else if (e.target.value == 200) {
+//         if (e.key == "ArrowUp") // e.target.value = parseInt(e.target.value) + 1;
+//         return e.target.step = 2;
+//     }  
+//     else if (e.target.value < 500) {
+//         return e.target.step = 2;
+//     } 
+//     //break value at 500
+//     else if (e.target.value == 500) {
+//         if (e.key == "ArrowDown") return e.target.step = 2;
+//         return e.target.step = 5;
+//     }
+    
+//     //break value at 2000
+//     else if (e.target.value == 2000) {
+//         if (e.key == "ArrowDown") return e.target.step = 5;
+//         return e.target.step = 10;
+//     }
+    
+//     else if (e.target.value < 2000) {
+//         return e.target.step = 5;
+//     } 
+    
+//     //break value at 5000
+//     else if (e.target.value == 5000) {
+//         if (e.key == "ArrowDown") return e.target.step = 5;
+//         return e.target.step = 10;
+//     }
+//     else if (e.target.value < 5000) {
+//         return e.target.step = 10;
+//     } 
+    
+//     else if (e.target.value > 5000 ) {
+//         return e.target.step = 25;
+//     } else {
+//         console.log("something error");
+//     }
+// });
